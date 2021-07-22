1ee8b129b659edf134e89853d9ef4bb1205ebda3
Cameron Hyde <c.hyde@qcif.edu.au>
2021-07-22 06:17:23 +0000

    WIP: setup for IT deployment

---
+++ files/galaxy/dynamic_job_rules/my_rules.py
@@ -0,0 +1,12 @@
+import os
+
+from galaxy.jobs import JobDestination
+from galaxy.jobs.mapper import JobMappingException
+
+
+def admin_only(app, user_email):
+    # Only allow the tool to be executed if the user is an admin
+    admin_users = app.config.get("admin_users", "").split(",")
+    if user_email not in admin_users:
+        raise JobMappingException("Unauthorized.")
+    return JobDestination(runner="slurm")
